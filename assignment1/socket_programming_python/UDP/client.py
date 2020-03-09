3bef0f6c6a00522687f58156e75f740a501c2c02
Oskari Tuormaa <oskaritu@gmail.com>
2020-03-09 15:06:59 +0100

    Added comments to code

--- assignment1/socket_programming_python/UDP/client.py
+++ assignment1/socket_programming_python/UDP/client.py
@@ -6,9 +6,12 @@ serverPort = int(sys.argv[2])
 msg = sys.argv[3]
 bufferSize = 256
 
+# Create and setup socket
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+# Send encoded message through socket
 UDPClientSocket.sendto(str.encode(msg), (serverAddress, serverPort))
 
+# Read and print response
 s = UDPClientSocket.recv(bufferSize)
 print(s.decode())
--- assignment1/socket_programming_python/UDP/server.py
+++ assignment1/socket_programming_python/UDP/server.py
@@ -6,10 +6,13 @@ localIP = "0.0.0.0" # Bind to every available IP
 localPort = int(sys.argv[1])
 bufferSize = 256
 
+# Create and setup socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+# Bind socket to IP
 UDPServerSocket.bind((localIP, localPort))
 
+# Open and get file descriptors for each file
 f_l = open("/proc/loadavg", "r")
 f_u = open("/proc/uptime", "r")
 
@@ -17,35 +20,40 @@ running = True
 print("UDP Server up and listening!")
 
 while(running):
+    # Check whether stdin or socket is ready to be read from
     ready, _, _ = select.select([UDPServerSocket, sys.stdin], [], [], 1)
 
+    # If socket has data
     if UDPServerSocket in ready:
+        # Read from socket
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
         msg = bytesAddressPair[0]
         address = bytesAddressPair[1]
 
+        # Decode received message
         msg = msg.decode()
 
         print("Received {} from {}.".format(msg, address))
 
-        if msg == "l" or msg == 'L':
+        if msg == "l" or msg == "L": # Received L or l
             response = f_l.read()
             f_l.seek(0)
             UDPServerSocket.sendto(response[:-1].encode(), address)
 
-        elif msg == "u" or msg == "U":
+        elif msg == "u" or msg == "U": # Received U or u
             response = f_u.read()
             f_u.seek(0)
             UDPServerSocket.sendto(response[:-1].encode(), address)
 
-        elif msg == "q" or msg == "Q":
-            running = False
+        elif msg == "q" or msg == "Q": # Received Q or q
+            running = False # Shutdown server
             UDPServerSocket.sendto(str.encode("Server shutting down"), address)
 
-        else:
+        else: # Received unknown command
             UDPServerSocket.sendto(str.encode("Unknown command"), address)
 
+    # If stdin has data
     if sys.stdin in ready:
         s = sys.stdin.readline()
-        if s == "q\n" or s == "Q\n":
-            running = False
+        if s == "q\n" or s == "Q\n": # Received q or Q through stdin
+            running = False # Shutdown server
