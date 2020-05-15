781b1fad7a676fb95a41f1175ba3ca5090d4740c
Oskari Tuormaa <oskaritu@gmail.com>
2020-05-15 18:34:40 +0200

    Added code to ensure observation data is proper

--- assignment3/observationServer/Models/Observation.cs
+++ assignment3/observationServer/Models/Observation.cs
@@ -2,15 +2,34 @@ using System;
 
 namespace observationServer.Models
 {
-  public class Observation
-  {
-    public int id { get; set; }
-    public DateTime Date { get; set; }           // Required
-    public string Name { get; set; }             // Required
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public float Temperature { get; set; }
-    public float Humidity { get; set; }
-    public float Pressure { get; set; }  
-  }
+    public class Observation
+    {
+        private float temperature;
+        private int humidity;
+        private float pressure;
+
+        public int id { get; set; }
+        public DateTime Date { get; set; }           // Required
+        public string Name { get; set; }             // Required
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public float Temperature
+        {
+            get { return temperature; }
+            set { temperature = (float)System.Math.Round(value, 1); } // Round value to 1 digit
+        }
+
+        public int Humidity
+        {
+            get { return humidity; }
+            set { humidity = (0 <= value && value <= 100) ? value : 0; } // Ensure that value is between 0-100
+        }
+
+        public float Pressure
+        {
+            get { return pressure; }
+            set { pressure = (float)System.Math.Round(value, 1); } // Round value to 1 digit
+        }
+    }
 }
